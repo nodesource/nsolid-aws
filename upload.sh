cd8b3d5b1fc45b5e5d2bacd6451547a94e080404
Graham Krizek <gkrizek@nodesource.com>
2018-02-12 15:12:55 -0700

    Update upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
 
 if [[ ! -e ~/.aws/credentials ]]; then
   echo "No credentials present for the AWS CLI"
   exit 1
 fi
+
+for file in templates/*.json; do
+  echo ""
+  echo "Validating $file"
+  #aws cloudformation validate-template --template-body file://$file
+  rc=$?
+  echo ""
+  if [[ $rc != 0 ]]; then
+    exit $rc
+  fi
+done
 
 BUCKET="nodesource-public-cloudformation"
 aws s3 sync ./templates s3://$BUCKET/nsolid/
