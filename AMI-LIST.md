e8826164377d66333395a694b2e020cb3dc9f9dd
Graham Krizek <gkrizek@nodesource.com>
2018-03-07 21:46:03 -0700

    Add AMI Mapping Update

--- AMI-LIST.md
+++ AMI-LIST.md
@@ -2,34 +2,30 @@
 
 ## us-east-1
 
-| NSolid Version |   Console AMI  |  Runtime AMI   |
-|----------------|----------------|----------------|
-|   **3.0.6**    | `ami-east1czz` | `ami-east1czz` |
-|   **3.0.5**    | `ami-123abc` | `ami-123abc` |
+| NSolid Version | Console AMI | Runtime AMI |
+|----------------|-------------|-------------|
+|   **3.0.4**    |  `am-123`   |  `ami-123`  |
 
 
 ## us-east-2
 
-| NSolid Version |   Console AMI  |  Runtime AMI   |
-|----------------|----------------|----------------|
-|   **3.0.6**    | `ami-east2czz` | `ami-east2czz` |
-|   **3.0.5**    | `ami-123abc` | `ami-123abc` |
+| NSolid Version | Console AMI | Runtime AMI |
+|----------------|-------------|-------------|
+|   **3.0.4**    |  `am-123`   |  `ami-123`  |
 
 
 ## us-west-1
 
-| NSolid Version |   Console AMI  |  Runtime AMI   |
-|----------------|----------------|----------------|
-|   **3.0.6**    | `ami-west1czz` | `ami-west1czz` |
-|   **3.0.5**    | `ami-123abc` | `ami-123abc` |
+| NSolid Version | Console AMI | Runtime AMI |
+|----------------|-------------|-------------|
+|   **3.0.4**    |  `am-123`   |  `ami-123`  |
 
 
 ## us-west-2
 
-| NSolid Version |   Console AMI  |  Runtime AMI   |
-|----------------|----------------|----------------|
-|   **3.0.6**    | `ami-west2czz` | `ami-west2czz` |
-|   **3.0.5**    | `ami-123ababc` | `ami-12ab3abc` |
+| NSolid Version | Console AMI | Runtime AMI |
+|----------------|-------------|-------------|
+|   **3.0.4**    |  `am-123`   |  `ami-123`  |
 
 
 ## ca-central-1
--- update_version.py
+++ update_version.py
@@ -10,32 +10,6 @@ consoleFile = raw_input("N|Solid Console File (full path):\n")
 runtimeFile = raw_input("N|Solid Runtime File (full path):\n")
 consoleContent = json.loads(open(consoleFile).read())
 runtimeContent = json.loads(open(runtimeFile).read())
-'''
-s3 = boto3.client('s3')
-consoleContents = s3.list_objects_v2(Bucket="ns-cloud-artifacts", MaxKeys=1, Prefix="nsolid/nsolid-console")
-runtimeContents = s3.list_objects_v2(Bucket="ns-cloud-artifacts", MaxKeys=1, Prefix="nsolid/nsolid-runtime")
-consolePreviousKey = consoleContents['Content'][0]['Key']
-consolePreviousKey = runtimeContents['Content'][0]['Key']
-cosnoleObj = s3.get_object(Bucket="ns-cloud-artifacts", Key=consolePreviousKey)
-runtimeObj = s3.get_object(Bucket="ns-cloud-artifacts", Key=runtimePreviousKey)
-consoleBody = consoleObj['Body'].read()
-runtimeBody = runtimeObj['Body'].read()
-previousConsole = json.loads(consoleBody.decode('utf-8'))
-previousRuntime = json.loads(runtimeBody.decode('utf-8'))
-with open(consoleFile, 'rb') as consoleData:
-    upload = s3.upload_fileobj(consoleData, 'ns-cloud-artifacts', 'nsolid/nsolid-console-' + nsolidVersion + '.json')
-with open(runtimeFile, 'rb') as runtimeData:
-    upload = s3.upload_fileobj(runtimeData, 'ns-cloud-artifacts', 'nsolid/nsolid-runtime-' + nsolidVersion + '.json')
-'''
-
-'''
-Create a folder in S3 that has past json file for earlier image versions.
-When this is run, upload the current file as well.
-
-AMI-LIST can probably still work the same way (minus the double headers).
-AMI Mappings will probably need to have a find and replace done on them,
-referencing the old AMI ID it got from S3, and replacing with the new ID.
-'''
 
 # AMI-LIST.md update
 if "**" + nsolidVersion + "**" in open('AMI-LIST.md').read():
@@ -69,3 +43,50 @@ with open('AMI-LIST.md', 'w') as originalFile:
 os.remove("./.AMI-LIST.updated.md")
 
 # AMI Maps update
+s3 = boto3.client('s3')
+consoleContents = s3.list_objects_v2(Bucket="ns-cloud-artifacts", MaxKeys=1, Prefix="nsolid/nsolid-console")
+runtimeContents = s3.list_objects_v2(Bucket="ns-cloud-artifacts", MaxKeys=1, Prefix="nsolid/nsolid-runtime")
+consolePreviousKey = consoleContents['Content'][0]['Key']
+consolePreviousKey = runtimeContents['Content'][0]['Key']
+cosnoleObj = s3.get_object(Bucket="ns-cloud-artifacts", Key=consolePreviousKey)
+runtimeObj = s3.get_object(Bucket="ns-cloud-artifacts", Key=runtimePreviousKey)
+consoleBody = consoleObj['Body'].read()
+runtimeBody = runtimeObj['Body'].read()
+previousConsole = json.loads(consoleBody.decode('utf-8'))
+previousRuntime = json.loads(runtimeBody.decode('utf-8'))
+with open(consoleFile, 'rb') as consoleData:
+    upload = s3.upload_fileobj(consoleData, 'ns-cloud-artifacts', 'nsolid/nsolid-console-' + nsolidVersion.replace('.', '') + '.json')
+with open(runtimeFile, 'rb') as runtimeData:
+    upload = s3.upload_fileobj(runtimeData, 'ns-cloud-artifacts', 'nsolid/nsolid-runtime-' + nsolidVersion.replace('.', '') + '.json')
+
+# Console Update
+for newConsole in consoleContent['imageIds']:
+    for oldConsole in previousConsole['imageIds']:
+        if newConsole['region'] == oldConsole['region']:
+            templates = os.listdir('./templates')
+            for template in templates:
+                if ".json" in template:
+                    with open("./templates/" + template, "r") as inFile:
+                        lines = inFile.readlines()
+                    with open("./templates/" + template, "w") as outFile:
+                        for line in lines:
+                            if oldConsole['ami'] in line:
+                                line.replace(oldConsole['ami'], newConsole['ami'])
+                            outFile.write(line)
+
+# Runtime Update
+for newRuntime in runtimeContent['imageIds']:
+    for oldRuntime in previousRuntime['imageIds']:
+        if newRuntime['region'] == oldRuntime['region']:
+            templates = os.listdir('./templates')
+            for template in templates:
+                if ".json" in template:
+                    with open("./templates/" + template, "r") as inFile:
+                        lines = inFile.readlines()
+                    with open("./templates/" + template, "w") as outFile:
+                        for line in lines:
+                            if oldRuntime['ami'] in line:
+                                line.replace(oldRuntime['ami'], newRuntime['ami'])
+                            outFile.write(line)
+
+print('Update Complete.')
